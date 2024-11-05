2610656b9160d004d128a0267088f7c447a32607
Dominik Remo <47261058+DominikRemo@users.noreply.github.com>
2024-11-05 13:54:24 +0100

    Convert images to jpeg and optimize them

--- playground/src/components/expert_evaluation/expert_view/exercise_screen.tsx
+++ playground/src/components/expert_evaluation/expert_view/exercise_screen.tsx
@@ -1,73 +1,73 @@
 import React, {useState} from 'react';
 import ExerciseDetailPopup from "@/components/expert_evaluation/expert_view/exercise_detail_popup";
 import {Exercise} from "@/model/exercise";
-import background_image from "@/assets/evaluation_backgrounds/exercise.webp";
-import {PrimaryButton, SecondaryButton} from "@/components/expert_evaluation/expert_evaluation_buttons";
+import background_image from "@/assets/evaluation_backgrounds/exercise.jpeg";
+import {SecondaryButton} from "@/components/expert_evaluation/expert_evaluation_buttons";
 
 
 interface ExerciseScreenProps {
-    onCloseExerciseDetail: () => void;
-    onOpenContinueLater: () => void;
-    exercise: Exercise;
-    currentExerciseIndex: number;
-    totalExercises: number;
+  onCloseExerciseDetail: () => void;
+  onOpenContinueLater: () => void;
+  exercise: Exercise;
+  currentExerciseIndex: number;
+  totalExercises: number;
 }
 
 const ExerciseScreen: React.FC<ExerciseScreenProps> = ({
-                                                           onCloseExerciseDetail,
-                                                           onOpenContinueLater,
-                                                           exercise,
-                                                           currentExerciseIndex,
-                                                           totalExercises
+                                                         onCloseExerciseDetail,
+                                                         onOpenContinueLater,
+                                                         exercise,
+                                                         currentExerciseIndex,
+                                                         totalExercises
                                                        }) => {
-    const [isExerciseDetailOpen, setExerciseDetailOpen] = useState(false);
+  const [isExerciseDetailOpen, setExerciseDetailOpen] = useState(false);
 
-    // Function to close the ExerciseDetailPopup
-    const closeExerciseDetail = () => {
-        setExerciseDetailOpen(false);
-        onCloseExerciseDetail();
-    }
+  // Function to close the ExerciseDetailPopup
+  const closeExerciseDetail = () => {
+    setExerciseDetailOpen(false);
+    onCloseExerciseDetail();
+  }
 
-    return (
-        <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
-             style={{
-                 backgroundImage: `url(${background_image.src})`, // Use the same path as in tailwind.config.js
-                 backgroundSize: 'cover',
-                 backgroundPosition: 'center',
-             }}
-        >
-            <div className="bg-white p-8 rounded-lg shadow-lg max-w-lg w-full text-center">
-                <h1 className="text-4xl font-bold mb-4">You have
-                    evaluated {currentExerciseIndex}/{totalExercises} exercises!</h1>
-                <p className="text-lg mb-6">
-                    Great job! Feel free to take a break and
-                    continue later. Your progress has been saved.
-                </p>
-                <p className="text-lg mb-6">
-                    When you are ready, continue with the next exercise: {exercise.title}.
-                </p>
-                <div className="flex justify-center mt-4">
-                    <SecondaryButton
-                        onClick={onOpenContinueLater}
-                        text="😴 Continue Later"
-                        className="px-6 py-3"
-                    />
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+         style={{
+           backgroundImage: `url(${background_image.src})`, // Use the same path as in tailwind.config.js
+           backgroundSize: 'cover',
+           backgroundPosition: 'center',
+         }}
+    >
+      <div className="bg-white p-8 rounded-lg shadow-lg max-w-lg w-full text-center">
+        <h1 className="text-4xl font-bold mb-4">You have
+          evaluated {currentExerciseIndex}/{totalExercises} exercises!</h1>
+        <p className="text-lg mb-6">
+          Great job! Feel free to take a break and
+          continue later. Your progress has been saved.
+        </p>
+        <p className="text-lg mb-6">
+          When you are ready, continue with the next exercise: {exercise.title}.
+        </p>
+        <div className="flex justify-center mt-4">
+          <SecondaryButton
+            onClick={onOpenContinueLater}
+            text="😴 Continue Later"
+            className="px-6 py-3"
+          />
 
-                    <SecondaryButton
-                        onClick={() => setExerciseDetailOpen(true)}
-                        text="📄 Exercise Details"
-                        className="ml-4 px-6 py-3"
-                    />
-                </div>
-            </div>
-            {/* ExerciseDetailPopup that shows up when isExerciseDetailOpen is true */}
-            <ExerciseDetailPopup
-                isOpen={isExerciseDetailOpen}
-                onClose={closeExerciseDetail}
-                exercise={exercise}
-            />
+          <SecondaryButton
+            onClick={() => setExerciseDetailOpen(true)}
+            text="📄 Exercise Details"
+            className="ml-4 px-6 py-3"
+          />
         </div>
-    );
+      </div>
+      {/* ExerciseDetailPopup that shows up when isExerciseDetailOpen is true */}
+      <ExerciseDetailPopup
+        isOpen={isExerciseDetailOpen}
+        onClose={closeExerciseDetail}
+        exercise={exercise}
+      />
+    </div>
+  );
 };
 
 export default ExerciseScreen;
