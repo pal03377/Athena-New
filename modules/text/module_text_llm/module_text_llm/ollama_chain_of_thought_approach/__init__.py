1f15a488390e452fedf276c68206e897fae31f4b
= Enea_Gore <ge72git@mytum.de>
2025-01-01 14:44:19 +0100

    graceful handling of failed ollama instantiation

--- modules/text/module_text_llm/module_text_llm/ollama_chain_of_thought_approach/__init__.py
+++ modules/text/module_text_llm/module_text_llm/ollama_chain_of_thought_approach/__init__.py
@@ -1,13 +1,16 @@
 from pydantic import BaseModel, Field
 from typing import Literal
-from llm_core.models import ModelConfigType, OllamaModelConfig
-
+from llm_core.models import ModelConfigType
+try:
+    from llm_core.models import OllamaModelConfig
+except ImportError as e:
+    print(f"Warning: Failed to import models. {e}")
+    OllamaModelConfig = None
 from module_text_llm.approach_config import ApproachConfig
 from module_text_llm.ollama_chain_of_thought_approach.prompt_generate_feedback import CoTGenerateSuggestionsPrompt
 from module_text_llm.ollama_chain_of_thought_approach.prompt_thinking import ThinkingPrompt
 
 class OllamaChainOfThoughtConfig(ApproachConfig):
-    # Defaults to the cheaper mini 4o model
     type: Literal['ollama_chain_of_thought'] = 'ollama_chain_of_thought'
     model: ModelConfigType = Field(default=OllamaModelConfig)  # type: ignore
     thikning_prompt: ThinkingPrompt = Field(default=ThinkingPrompt())
