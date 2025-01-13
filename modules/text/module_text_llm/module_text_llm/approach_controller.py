119cf6b9e5d224c0bedbb3cbeaf683a59d6a3e1c
= Enea_Gore <ge72git@mytum.de>
2025-01-13 16:15:08 +0100

    comment

--- modules/text/module_text_llm/module_text_llm/approach_controller.py
+++ modules/text/module_text_llm/module_text_llm/approach_controller.py
@@ -4,7 +4,8 @@ from module_text_llm.approach_config import ApproachConfig
 
 async def generate_suggestions(exercise: Exercise, submission: Submission, config: ApproachConfig, debug: bool) -> List[Feedback]:
     # Importing the strategy_factory here to avoid circular imports with ApproachConfig
-    from module_text_llm import strategy_factory # pylint: disable=import-outside-toplevel
+    # pylint: disable=import-outside-toplevel
+    from module_text_llm import strategy_factory 
 
     strategy = strategy_factory.get_strategy(config)
     return await strategy.generate_suggestions(exercise, submission, config, debug)
