c150612ee0f0a14527b7356540dc803097dd5974
= Enea_Gore <ge72git@mytum.de>
2025-01-13 16:05:58 +0100

    move approach discovery and strategyFactory to athena package

--- modules/text/module_text_llm/module_text_llm/__init__.py
+++ modules/text/module_text_llm/module_text_llm/__init__.py
@@ -1,3 +1,6 @@
 import dotenv
+from athena.approach_discovery.strategy_factory import SuggestionStrategyFactory
+from module_text_llm.approach_config import ApproachConfig
 
 dotenv.load_dotenv(override=True)
+strategyFactory = SuggestionStrategyFactory("module_text_llm", ApproachConfig)
