17c7fd015d687e961d6c899351f6f8d5099b0183
= Enea_Gore <ge72git@mytum.de>
2025-01-14 17:49:11 +0100

    quick fix

--- modules/text/module_text_llm/module_text_llm/chain_of_thought_approach/__init__.py
+++ modules/text/module_text_llm/module_text_llm/chain_of_thought_approach/__init__.py
@@ -8,7 +8,6 @@ from module_text_llm.chain_of_thought_approach.prompt_thinking import ThinkingPr
 
 class ChainOfThoughtConfig(ApproachConfig):
     type: Literal['chain_of_thought'] = 'chain_of_thought'
-    model: ModelConfigType = Field(default=DefaultModelConfig)  # type: ignore
     thikning_prompt: ThinkingPrompt = Field(default=ThinkingPrompt())
     generate_suggestions_prompt: CoTGenerateSuggestionsPrompt = Field(default=CoTGenerateSuggestionsPrompt())
     
