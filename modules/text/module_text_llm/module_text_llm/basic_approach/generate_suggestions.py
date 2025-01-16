20092fb120c2830fa7af5556ddf473c3ae4033ab
Maximilian Sölch <maximiliansoelch@users.noreply.github.com>
2025-01-16 10:45:14 +0100

    Add missing whitespace

--- modules/text/module_text_llm/module_text_llm/basic_approach/generate_suggestions.py
+++ modules/text/module_text_llm/module_text_llm/basic_approach/generate_suggestions.py
@@ -14,7 +14,7 @@ from module_text_llm.config import BasicApproachConfig
 from module_text_llm.helpers.utils import add_sentence_numbers, get_index_range_from_line_range, format_grading_instructions
 from module_text_llm.basic_approach.prompt_generate_suggestions import AssessmentModel
 
-async def generate_suggestions(exercise: Exercise, submission: Submission, config: BasicApproachConfig, debug: bool,is_graded: bool) -> List[Feedback]:
+async def generate_suggestions(exercise: Exercise, submission: Submission, config: BasicApproachConfig, debug: bool, is_graded: bool) -> List[Feedback]:
     model = config.model.get_model()  # type: ignore[attr-defined]
     prompt_input = {
         "max_points": exercise.max_points,
