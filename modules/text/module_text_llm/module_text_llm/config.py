139be516a0bac060317a237d32b7991444221437
= Enea_Gore <ge72git@mytum.de>
2025-01-20 02:13:32 +0100

    merge icl approach

--- modules/text/module_text_llm/module_text_llm/config.py
+++ modules/text/module_text_llm/module_text_llm/config.py
@@ -6,8 +6,8 @@ from athena import config_schema_provider
 from module_text_llm.chain_of_thought_approach import ChainOfThoughtConfig
 from module_text_llm.basic_approach import BasicApproachConfig
 from module_text_llm.ollama_chain_of_thought_approach import OllamaChainOfThoughtConfig
-
-ApproachConfigUnion = Union[BasicApproachConfig, ChainOfThoughtConfig, OllamaChainOfThoughtConfig, CouncilOfLlamasConfig]
+from module_text_llm.icl_rag_approach import IclRagApproachConfig
+ApproachConfigUnion = Union[BasicApproachConfig, ChainOfThoughtConfig, OllamaChainOfThoughtConfig, CouncilOfLlamasConfig,IclRagApproachConfig]
 
 @config_schema_provider
 class Configuration(BaseModel):
