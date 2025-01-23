87611430ab9510243099df27c7d34e3945f9ffc5
= Enea_Gore <ge72git@mytum.de>
2025-01-23 04:56:22 +0100

    merge security

--- modules/text/module_text_llm/module_text_llm/__main__.py
+++ modules/text/module_text_llm/module_text_llm/__main__.py
@@ -11,6 +11,8 @@ from module_text_llm.config import Configuration
 from module_text_llm.evaluation import get_feedback_statistics, get_llm_statistics
 from module_text_llm.generate_evaluation import generate_evaluation
 from module_text_llm.approach_controller import generate_suggestions
+from module_text_llm.helpers.detect_suspicios_submission import hybrid_suspicion_score, llm_check
+
 #Test Demo
 @submissions_consumer
 def receive_submissions(exercise: Exercise, submissions: List[Submission]):
@@ -31,6 +33,15 @@ def process_incoming_feedback(exercise: Exercise, submission: Submission, feedba
 async def suggest_feedback(exercise: Exercise, submission: Submission, is_graded: bool, module_config: Configuration) -> List[Feedback]:
     logger.info("suggest_feedback: %s suggestions for submission %d of exercise %d were requested, with approach: %s",
                 "Graded" if is_graded else "Non-graded", submission.id, exercise.id, module_config.approach.__class__.__name__)
+    logger.info("suggest_feedback: %s suggestions for submission %d of exercise %d were requested",
+                "Graded" if is_graded else "Non-graded", submission.id, exercise.id)
+    is_sus, score = hybrid_suspicion_score(submission.text, threshold=0.8)
+    if is_sus:
+        logger.info("Suspicious submission detected with score %f", score)
+        is_suspicious,suspicios_text = await llm_check(submission.text)
+        if is_suspicious:
+            logger.info("Suspicious submission detected by LLM with text %s", suspicios_text)
+            return [Feedback(title="Instructors need to review this submission", description="This Submission potentially violates the content policy!", credits=-1.0, exercise_id=exercise.id, submission_id=submission.id, is_graded=is_graded)]
     return await generate_suggestions(exercise, submission, module_config.approach, module_config.debug, is_graded)
 
 
