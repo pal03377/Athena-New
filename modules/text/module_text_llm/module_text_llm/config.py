7e5197d0b0ba1da7f2b2a3accfd9a32a10381afc
= Enea_Gore <ge72git@mytum.de>
2025-01-27 17:04:39 +0100

    inital implementation

--- modules/text/module_text_llm/module_text_llm/config.py
+++ modules/text/module_text_llm/module_text_llm/config.py
@@ -1,10 +1,10 @@
-from module_text_llm.divide_and_conquer import DivideAndConquerConfig
 from pydantic import BaseModel, Field
 from typing import Union
 from athena import config_schema_provider
 
 from module_text_llm.chain_of_thought_approach import ChainOfThoughtConfig
 from module_text_llm.basic_approach import BasicApproachConfig
+from module_text_llm.divide_and_conquer import DivideAndConquerConfig
 
 ApproachConfigUnion = Union[BasicApproachConfig, ChainOfThoughtConfig, DivideAndConquerConfig]
 
--- modules/text/module_text_llm/module_text_llm/divide_and_conquer/prompt_generate_suggestions.py
+++ modules/text/module_text_llm/module_text_llm/divide_and_conquer/prompt_generate_suggestions.py
@@ -1,32 +1,6 @@
 from pydantic import Field, BaseModel
 from typing import List, Optional
 
-system_message = """You are an AI tutor for text assessment at a prestigious university.
-
-# Task
-Create graded feedback suggestions for a student's text submission that a human tutor would accept. Meaning, the feedback you provide should be applicable to the submission with little to no modification.
-
-# Style
-1. Constructive, 2. Specific, 3. Balanced, 4. Clear and Concise, 5. Actionable, 6. Educational, 7. Contextual
-
-# Problem statement
-{problem_statement}
-
-# Example solution
-{example_solution}
-
-# Grading instructions
-{grading_instructions}
-Max points: {max_points}, bonus points: {bonus_points}
-    
-Respond in json.
-"""
-
-human_message = """Student's submission to grade (with sentence numbers <number>: <sentence>):
-
-{submission}
-"""
-
 # Input Prompt
 class GenerateSuggestionsPrompt(BaseModel):
     """Features available: **{problem_statement}**, **{example_solution}**, **{grading_instructions}**, **{max_points}**, **{bonus_points}**, **{submission}**
