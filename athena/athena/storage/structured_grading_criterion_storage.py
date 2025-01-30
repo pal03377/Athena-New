f6259c468fdf26961d08c633480de269ab62b544
LeonWehrhahn <leon.wehrhahn@protonmail.ch>
2025-01-30 15:32:06 +0100

    Remove lms_url from DBStructuredGradingCriterion and update related queries

--- athena/athena/storage/structured_grading_criterion_storage.py
+++ athena/athena/storage/structured_grading_criterion_storage.py
@@ -8,8 +8,9 @@ from athena.schemas import StructuredGradingCriterion
 def get_structured_grading_criterion(exercise_id: int, current_hash: Optional[str] = None) -> Optional[StructuredGradingCriterion]:
     lms_url = get_lms_url()
     with get_db() as db:
-        cache_entry = db.query(DBStructuredGradingCriterion).filter_by(
-            exercise_id=exercise_id, lms_url=lms_url
+        cache_entry = db.query(DBStructuredGradingCriterion).filter(
+            DBStructuredGradingCriterion.exercise_id == exercise_id,
+            DBStructuredGradingCriterion.exercise.has(lms_url=lms_url)
         ).first()
         if cache_entry is not None and (current_hash is None or cache_entry.instructions_hash == current_hash):  # type: ignore
             return StructuredGradingCriterion.parse_obj(cache_entry.structured_grading_criterion)
@@ -18,12 +19,10 @@ def get_structured_grading_criterion(exercise_id: int, current_hash: Optional[st
 def store_structured_grading_criterion(
     exercise_id: int, hash: str, structured_instructions: StructuredGradingCriterion
 ):
-    lms_url = get_lms_url()
     with get_db() as db:
         db.merge(
             DBStructuredGradingCriterion(
                 exercise_id=exercise_id,
-                lms_url=lms_url,
                 instructions_hash=hash,
                 structured_grading_criterion=structured_instructions.dict(),
             )
