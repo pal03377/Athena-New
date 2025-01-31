e060cd1b5c04ba8470afb1b7404a8ccc9fa3ecb3
LeonWehrhahn <leon.wehrhahn@protonmail.ch>
2025-01-31 21:13:51 +0100

    Add AzureModelConfig to ModelConfigType for multi-provider support

--- llm_core/llm_core/models/__init__.py
+++ llm_core/llm_core/models/__init__.py
@@ -1,6 +1,7 @@
 from typing import Union
 
+from llm_core.models.providers.azure_model_config import AzureModelConfig
 from llm_core.models.providers.openai_model_config import OpenAIModelConfig
 
 
-ModelConfigType = Union[OpenAIModelConfig]
+ModelConfigType = Union[OpenAIModelConfig, AzureModelConfig]
